a54439c1aac747b5ec8d4dff7d895576289e739d
regevbs <regevbensi@gmail.com>
2018-08-23 19:45:38 +0300

    try 2 get error again using set key 33

--- client.c
+++ client.c
@@ -1157,7 +1157,7 @@ int main(int argc, char *argv[])
     memset(send_buffer, 'x', MAX_TEST_SIZE - 1);
     assert(0 == set(dkvHandle, "1", send_buffer));
     assert(0 == get(dkvHandle, "1", &recv_buffer));
-    assert(0 == set(dkvHandle, "1", send_buffer));
+    assert(0 == set(dkvHandle, "33", send_buffer));
     assert(0 == strcmp(send_buffer, recv_buffer));
     
     release(recv_buffer);
